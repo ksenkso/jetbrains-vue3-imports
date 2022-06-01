38246d13422698cb34c4f6eaeeec55fa6122641a
GitHub Action <action@github.com>
2022-06-01 08:02:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ksenkso/jetbrainsvue3imports/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ksenkso.jetbrainsvue3imports.services
+
+import com.github.ksenkso.jetbrainsvue3imports.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
